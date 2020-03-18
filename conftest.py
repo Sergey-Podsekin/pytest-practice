567eaada29bf78487d131dac98623d8d80dca76e
Sergey Podsekin <sergeykenzo@gmail.com>
2020-03-18 21:25:40 +0300

    Maximized window mode

--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 def browser():
     driver = webdriver.Chrome()
     driver.get("http://automationpractice.com/index.php")
+    driver.maximize_window()
     driver.implicitly_wait(5)
     yield driver
     driver.quit()
