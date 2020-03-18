e7b8e9ef2200f01215cd5cad80d6b011465c56e3
Sergey Podsekin <sergeykenzo@gmail.com>
2020-03-18 21:25:01 +0300

    Added locator for pop-up, optimized xpath

--- locators/locators_cart_page.py
+++ locators/locators_cart_page.py
@@ -2,6 +2,8 @@ class Cart_page_locators:
     empty_cart = "//*[@class='alert alert-warning']"
     cart_summary_message = "//*[@id='cart_title']"
     your_shopping_cart = "//*[@class = 'breadcrumb clearfix']"
-    add_to_cart = '//*[@id="homefeatured"]/li[1]/div/div[2]/' \
-                  'div[2]/a[1]/span'
+    add_to_cart = "//*[@id='homefeatured']/li[1]//*[contains" \
+                  "(text(),'Add to cart')]"
     product_in_cart = '//*[@id="summary_products_quantity"]'
+    first_item = "//*[@id='homefeatured']/li[1]/div/div[2]"
+    proceed_to_checkout = "//*[@id='layer_cart']//a"
